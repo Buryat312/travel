1385a970d18401b6e97b61e2e542d33765e255e1
Sultanbek <sultanbek.asakeev@gmail.com>
2021-08-19 20:50:24 +0600

    update

--- guide/views.py
+++ guide/views.py
@@ -40,3 +40,21 @@ class TravelDestroyGenericView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated, ]
     queryset = Travel.objects.all()
     lookup_field = 'id'
+
+# from rest_framework import serializers
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from .models import Travel
+# from .serializers import TravelSerializer
+
+# class TravelAPIView(APIView):
+    
+
+ 
+
+#     def get(self, request):
+#         travels = Travel.objects.all()
+#         serializer = TravelSerializer(travels, many=True)
+
+#         return Response(serializer.data)
+
